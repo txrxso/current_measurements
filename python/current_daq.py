be1e38ec33d8c253508d8f8d4b1bb6fbe72f41ea
txrxso <teresaqwu@gmail.com>
2026-02-19 11:33:06 -0800

    changed COM_7 to COM7 and removed config.py b/c relative import error

--- python/current_daq.py
+++ python/current_daq.py
@@ -4,13 +4,35 @@ import serial
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from .configs import Topology, Scenario
+from enum import Enum
+
+
+class Topology(Enum):
+    GATEWAY_ONLY = (1, "Gateway node only")
+    GATEWAY_NOISE = (2, "Gateway and noise node")
+    GATEWAY_AIR = (3, "Gateway and air quality node")
+    GATEWAY_FULL = (4, "Gateway, noise, and air quality node")
+
+    def __init__(self, id, description):
+        self.id = id
+        self.description = description
+
+class Scenario(Enum):
+    HEARTBEATS_ONLY = ("A", "Heartbeats only (5m)")
+    MOCK_ALERTS_12M = ("B", "Heartbeats (5m) + Mock Alerts (12m)")
+    MOCK_ALERTS_5M = ("C", "Heartbeats (5m) + Mock Alerts (5m)")
+
+    def __init__(self, code, description):
+        self.code = code
+        self.description = description
+
+
 
 # MANUALLY SET THESE VALUES
 topology = Topology.GATEWAY_ONLY
 scenario = Scenario.HEARTBEATS_ONLY
 baud = 115200
-port = "COM_3"          
+port = "COM7"          
 
 def pre_log_checks() -> str: 
     # ensure directories exist (../data/ and ../data/plots/)
